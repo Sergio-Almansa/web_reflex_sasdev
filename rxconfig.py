f6ef31077b30879fb30cb22c66112140192c0063
Sergio Almansa Saura <66486497+Sergio-Almansa@users.noreply.github.com>
2026-08-17 14:08:15 +0200

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,10 +1,6 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="web_reflex_sasdev",
+    app_name="sasdev_web_new",
     api_url="https://api.sergioalmansa.dev",
-    plugins=[
-        rx.plugins.SitemapPlugin(),
-        rx.plugins.TailwindV4Plugin(),
-    ]
 )
